6f7d0cccb0ad1dbb7dadd6e72b21dcfa9aa52791
Maria Laura <marialaurasmc@icloud.com>
2021-08-17 12:57:06 -0300

    criado um link no index.html, depois selecionado este elemento no arquivo main.js e adicionado o evento de click com um callback que seleciona o elemento app e alterna entre a adicao ou remocao da classe active a ele. No style.css foi atribuida a propriedade visibilidade esconder ao elemento app que tivesse tambem a classe active

--- src/main.dev.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-require("./style.css");
-
-document.querySelector('.app').innerHTML = "\n  <h1>B. Academy</h1>\n  <p>Boas vindas \xE0 semana de pr\xE9-work para o Bootcamp em React.js \uD83D\uDE01</p>\n";
-//# sourceMappingURL=main.dev.js.map
--- src/main.js
+++ src/main.js
@@ -4,3 +4,12 @@ document.querySelector('.app').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+const link = document.querySelector('[data-js="link"]');
+
+function toggleAppVisibility (event){
+  const app = document.querySelector('[data-js="app"]');
+  app.classList.toggle('active');
+}
+
+link.addEventListener('click', toggleAppVisibility);
