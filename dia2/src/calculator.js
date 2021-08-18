6e20a5da29c5a3903769286e5b9c0b1006e83b05
Maria Laura <marialaurasmc@icloud.com>
2021-08-18 14:59:45 -0300

    adicionado o operador como parametro da calculadora, o operador por sua vez executa a funcao designada utilizando os valores a e b informados como argumento

--- dia2/src/calculator.js
+++ dia2/src/calculator.js
@@ -1,12 +1,28 @@
-function calculadora (callback) {
-}
+  function add(a,b){
+  return a + b;
+  }
+  function subtract(a,b){
+  return a - b;
+  }
+  function multiply(a,b){
+  return a * b;
+  }
+  function divide(a,b){
+  return a / b;
+  }
 
-const sum = calculadora((a, b) => a + b)
-const sub = calculadora((a, b) => a - b)
-const mult = calculadora((a, b) => a * b)
-const div = calculadora((a, b) => a / b)
+  function calculadora (a, b, operador) {
+    return operador(a,b);
+  }
 
-console.log('Somar 1 e 2 = 3:', sum(1, 2) === 3)
-console.log('Subtrair 4 de 20 = 16:', sub(20, 4) === 16)
-console.log('Multiplicar 3 com 3 = 9:', mult(3, 3) === 9)
-console.log('Dividir 15 por 5 = 3:', div(15, 5) === 3)
+  const sum = calculadora(1, 2, add)
+  const sub = calculadora(20, 4, subtract)
+  const mult = calculadora(3, 3, multiply)
+  const div = calculadora(15, 5, divide)
+
+
+  console.log('Somar 1 e 2 = 3:', sum === 3)
+  console.log('Subtrair 4 de 20 = 16:', sub === 16)
+  console.log('Multiplicar 3 com 3 = 9:', mult === 9)
+  console.log('Dividir 15 por 5 = 3:', div === 3)
+
