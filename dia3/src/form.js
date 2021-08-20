bbe3c2cb9d522cce13e6e712f0c61fd94c506340
Maria Laura <marialaurasmc@icloud.com>
2021-08-20 09:03:12 -0300

    adicionado elemento select com multiple e options

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -23,3 +23,42 @@ inputNome.addEventListener('input', maskName);
 function captalize(nome){
   return nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase();
 };
+
+
+const form = document.querySelector('[data-js="form"]');
+
+const select = document.createElement('select');
+form.appendChild(select);
+select.setAttribute('multiple', '');
+
+const colorOptions = ['#0099e5', '#511378', '#ff6600', '#666666', '#e2001a'];
+
+colorOptions.forEach(color => {
+  const option = document.createElement('option');
+  option.value = color;
+  option.textContent = color;
+  select.appendChild(option);
+});
+
+const colorBox
+
+// const optionBlue = {
+//   name: 'blue',
+//   hex: '#0099e5'
+// }
+
+// const optionPurple = {
+//   name: 'purple',
+//   hex: '#511378'
+// }
+
+// const optionOrange = {
+//   name: 'orange',
+//   hex: '#ff6600'
+// }
+
+
+
+
+
+
